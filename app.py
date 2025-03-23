dcd9644570cca0a321abeb5c72e65acc347d4d6c
CYBERTECH <micfor722@gmail.com>
2025-03-23 13:36:43 -0400

    model

---
+++ app.py
@@ -0,0 +1,9 @@
+# import streamlit as st
+# import pickle 
+# import requests
+
+# #Funtion to fetch movie postart using movie ID
+
+# def fetch_poster(movie_id):{
+#     url= 'https//api.themoviedb.org/3/movie/{}api_keys'
+# }c  
